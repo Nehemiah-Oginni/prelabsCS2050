9546a1385932f5c081991b2cace56fc46cb079a1
Nehemiah-Oginni <neheoginni2@gmail.com>
2025-01-30 17:32:21 -0600

    finished prelab1 and got practice with arrays

---
+++ mallocArrays.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int findArraySize(int *array)
+{
+    printf("array size is %d\n", array[-1]);
+    
+    for(int i=0; i < array[-1]; i++)
+    {
+        printf("array[%d] is %d\n", i, array[i]);
+    }
+}
+
+
+
+int main (void)
+{
+    int size = (rand() % (1 - 10 + 1)) + 1; // (rand() % (max - min + 1)) + min
+    int *array = malloc(size * sizeof(int) * sizeof(int));
+    array[0] = size;
+    array++;
+    for(int i=0; i<size; i++)
+    {
+        int randnum = rand() % 10;
+        array[i] = randnum;
+    }
+    findArraySize(array);
+}
--- prelab1.c
+++ prelab1.c
@@ -1,3 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Implement a function that returns information relating to a dataset of integers in an array. Specifically,
+the function must provide the total number of distinct values (i.e., non-duplicates) in the array and the
+smallest value and largest value in the array. Below is a partial prototype for the function. You are free
+to determine the full prototype, e.g., other parameters and return value (if there is one).
+
+HINT: The user of this function will expect three pieces of information from it, and since you can’t have
+three return values, you’ll need to pass some or all the information to the user by reference.
+*/
+
+int getArrayInfo(int *array, int n, int* minval, int *maxval)// n is the size of array
+{
+    if(n != 0)
+    {
+        int max = array[0];
+        int min = array[0];
+        int total = 0;
+        for(int i=0; i<n; i++)
+        {
+            if(array[i] > max)
+            {
+                max = array[i];
+            }
+            if(array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        *minval = min;
+        *maxval = max;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<= n-1; j++)
+            {
+                printf(" array [%d]= %d,  array [%d]= %d\n ", i, array[i], j, array[j]);
+                if(array[i] == array[j])
+                {
+                    break;
+                }
+                if(j == n-1)
+                {
+                    total += 1;
+                }
+            }
+            
+        }
+        return total + 1;
+    }
+    return 0;
+}
+
+int main (void)
+{
+    int n = 0;
+    int array[n];
+    int min = 20;
+    int max = 30;
+    for(int i=0; i<n; i++)
+    {
+        array[i]=-5;
+    }
+
+
+    int total = getArrayInfo(array, n, &min, &max);
+
+    printf("max is %d, min is %d and there are %d\n", max, min, total);
+}
+
+//as you go through the array you want to compare each element to the current element
